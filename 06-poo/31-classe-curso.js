103ee6b568aa25a85915dee19e3132588cd30215
ArthurTardin <arthurtgciongoli@gmail.com>
2026-08-25 15:23:43 -0300

    feat: add resolution exercises 31

--- 06-poo/31-classe-curso.js
+++ 06-poo/31-classe-curso.js
@@ -5,3 +5,20 @@
 //
 // Escreva sua solução abaixo:
 
+class Curso{
+    constructor(nome, duracao, modalidade){
+        this.nome = nome;
+        this.duracao = duracao;
+        this.modalidade = modalidade;
+    }
+
+    exibirInformacoes(){
+        return `${this.nome}, ${this.duracao}, ${this.modalidade}`;
+    }
+}
+
+const curso1 = new Curso("Metalurgia", 2200, "não sei");
+const curso2 = new Curso("ADM", 2200, "Também não sei");
+
+console.log(curso1.exibirInformacoes());
+console.log(curso2.exibirInformacoes());
