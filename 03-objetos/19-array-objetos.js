580f29b9869c2caa930585387e681538a3c3415d
ArthurTardin <arthurtgciongoli@gmail.com>
2026-08-25 13:48:24 -0300

    feat: add resolution exercises 19

--- 03-objetos/19-array-objetos.js
+++ 03-objetos/19-array-objetos.js
@@ -5,3 +5,11 @@
 //
 // Escreva sua solução abaixo:
 
+const conjunto = [
+    {"titulo": 'God of War', "plataforma": 'PS2', "classificacao": 18},
+    {"titulo": 'Resident Evil', "plataforma": 'PS2', "classificacao": 18},
+    {"titulo": 'Valorant', "plataforma": 'PC', "classificacao": 16},
+    {"titulo": 'Cities Skyline', "plataforma": 'PC', "classificacao": 12},
+];
+
+const Percorra = conjunto.forEach((a) => console.log(`Nome do jogo: ${a.titulo}, plataforma: ${a.plataforma}, classificação: ${a.classificacao}`));
