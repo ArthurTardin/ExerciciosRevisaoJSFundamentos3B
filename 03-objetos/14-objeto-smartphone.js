919d451fa7bbc69bec3446d1fe08371165fc89cb
ArthurTardin <arthurtgciongoli@gmail.com>
2026-08-25 13:46:57 -0300

    feat: add resolution exercises 14

--- 03-objetos/14-objeto-smartphone.js
+++ 03-objetos/14-objeto-smartphone.js
@@ -4,3 +4,15 @@
 //
 // Escreva sua solução abaixo:
 
+class Smartphone{
+    constructor(marca, modelo, armazenamento, ligado){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.armazenamento = armazenamento;
+        this.ligado = ligado;
+    };
+}
+
+const smartphone = new Smartphone("Samsung", "S24", "Não sei", true);
+
+console.log(smartphone);
