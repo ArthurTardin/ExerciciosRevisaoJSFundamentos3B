81dbd07d6131687155814277da4ac94665b69c70
ArthurTardin <arthurtgciongoli@gmail.com>
2026-08-25 15:24:44 -0300

    feat: add resolution exercises 34

--- 06-poo/34-heranca-produto.js
+++ 06-poo/34-heranca-produto.js
@@ -5,3 +5,30 @@
 //
 // Escreva sua solução abaixo:
 
+class Produto {
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    exibirDados() {
+        return `${this.nome}, ${this.preco}`;
+    }
+}
+
+class ProdutosDigitais extends Produto {
+    constructor(nome, preco, tamanho) {
+        super(nome, preco);
+        this.tamanho = tamanho;
+    }
+
+    exibirDados() {
+        return `${this.nome}, ${this.preco}, ${this.tamanho}`;
+    }
+}
+
+const cursoCompleto = new Produto("Curso Avançado de JS", 150);
+console.log(cursoCompleto.exibirDados()); 
+
+const ebooK = new ProdutosDigitais("Livro de Node.js", 45, "12MB");
+console.log(ebooK.exibirDados());
