f867d2036ddb54331d7202115b9bdd94d40ed7ef
ArthurTardin <arthurtgciongoli@gmail.com>
2026-08-25 15:23:56 -0300

    feat: add resolution exercises 32

--- 06-poo/32-classe-carteira.js
+++ 06-poo/32-classe-carteira.js
@@ -5,3 +5,37 @@
 //
 // Escreva sua solução abaixo:
 
+class CarteiraDigital {
+    #saldo;
+
+    constructor() {
+        this.#saldo = 0;
+    }
+
+    adicionarCredito(valor) {
+        if (valor > 0) {
+            this.#saldo += valor;
+            return this.#saldo;
+        } else {
+            return "Valor inválido";
+        }
+    }
+
+    realizarPagamento(valor) {
+        if (valor > 0 && valor <= this.#saldo) {
+            this.#saldo -= valor;
+            return this.#saldo;
+        } else {
+            return "Valor inválido";
+        }
+    }
+
+    consultarSaldo() {
+        return this.#saldo;
+    }
+}
+
+const minhaCarteira = new CarteiraDigital();
+minhaCarteira.adicionarCredito(100);
+minhaCarteira.realizarPagamento(40);
+console.log(minhaCarteira.consultarSaldo());
