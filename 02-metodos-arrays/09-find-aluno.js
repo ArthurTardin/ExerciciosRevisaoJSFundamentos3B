3da984b1f0c06dec7c8b0661eff7ecf37af25b04
ArthurTardin <arthurtgciongoli@gmail.com>
2026-08-25 13:15:40 -0300

    feat: add resolution exercises 09

--- 02-metodos-arrays/09-find-aluno.js
+++ 02-metodos-arrays/09-find-aluno.js
@@ -5,3 +5,13 @@
 //
 // Escreva sua solução abaixo:
 
+const alunos = [
+    {nome: "Arthur", matricula: 100},
+    {nome: "Carlos", matricula: 102},
+    {nome: "Pablo", matricula: 103},
+    {nome: "Enzo", matricula: 104},
+]
+
+const aluno = alunos.find((aluno) => aluno.matricula === 103);
+
+console.log(aluno);
